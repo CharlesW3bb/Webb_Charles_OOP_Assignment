c8f6c69f2707dfe308bab795e72e110840c878b7
Samuel-Harwood <101341494+Samuel-Harwood@users.noreply.github.com>
2022-04-06 15:24:48 +0100

    Samuel Harwood Code Review
    Removed a class
    Updated naming convention
    Small code tweaks for readable code

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿//new
+//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment1_Charles
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Local list of integers to hold the first five measurements of the text
+            //CHalres
+            List<int> parameters = new List<int>();
+            string[] text;
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+
+            Input input = new Input();
+            text = input.GetInput();
+
+
+            //Create an 'Analyse' object
+
+            Analyse analyse = new Analyse();
+
+            //Pass the text input to the 'analyseText' method
+            List<int> results = new List<int>();
+            results = analyse.analyseText(text);
+
+
+            //Report the results of the analysis
+            Report report = new Report();
+            report.ReportAnalysis(results);
+            report.ReportFrequency(text[0].ToString());
+
+            //TO ADD: Get the frequency of individual letters? THIS IS COMPLETED IN ANALYSIS AND OUTPUTED IN REPORT
+
+
+        }
+
+
+
+    }
+}
---
+++ Validate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment1_Charles
+{
+    class Validate
+    {
+        private string userInput;//data abstarction. User input definiatly should not be accesable to other classes.
+
+
+        public string ExceptionCheck(string message) //checks if size of input causes unwanted/unexpected behaviour such as run time errors.
+                                                     //This method is used on manual inputs to see if any exceptions are made.
+        {
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(message);
+                    userInput = Console.ReadLine();
+                    return (userInput);
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect input, please try again");
+                }
+
+            }
+
+        }
+
+        public bool ExceptionCheck(string message, string file)
+        { //METHOD OVERLOADING is being used here to increace code readability.
+            //This method is used for files to check for any exceptions
+
+            return (false);
+        }
+
+        public string IsDirectoryValid()
+        {
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the file directory you wish to analyse");
+                    string inputDirectory = Console.ReadLine();
+                    string text = System.IO.File.ReadAllText(inputDirectory);
+                    return text;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{ e.Message} \n\nplease try again: \n");
+
+                }
+            }
+
+        }
+        public string ValidateManualOrFile(string message)
+        {
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"{message}"); // message is a passed parametre. It is used so that this one method can be used
+                    //for multiple diffrent purposes, meaning new validation methods dont need to be created.
+                    userInput = Console.ReadLine(); //If the user enters an extreme value that causes an error, it will be handled,
+                                                    //the return wont be run until a valid input is given.
+
+                }
+                catch
+                {
+                    Console.WriteLine("You entered incorrect input"); //gives feedback to the user that their input was invalid. 
+                }
+
+                if (userInput.ToLower() == "manual" || userInput.ToLower() == "file")
+                {
+                    return userInput;
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+}
