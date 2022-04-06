23ef77c394bf51315590740f551ed7d46e52fe67
CharlesW3bb <25757569@students.lincoln.ac.uk>
2022-04-06 14:21:56 +0100

    Base Code
    Very good.

---
+++ OOP-Assignment1-Charles/OOP-Assignment1-Charles/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment1_Charles
+{
+    class Report
+    {
+        //Handles the reporting of the analysis
+        //Maybe have different methods for different formats of output?
+        //eg.   public void outputConsole(List<int>)
+
+    }
+}
---
+++ OOP-Assignment1-Charles/OOP-Assignment1-Charles/Validation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment1_Charles
+{
+    class Validation
+    {
+    }
+}
